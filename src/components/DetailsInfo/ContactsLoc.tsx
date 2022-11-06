339058beaff7aebe9353a54b81940c4d3f205e1d
Max-Loktionov <studreno@gmail.com>
2022-11-06 23:39:43 +0200

    fix: fix styles and eslint

--- src/components/DetailsInfo/ContactsLoc.tsx
+++ src/components/DetailsInfo/ContactsLoc.tsx
@@ -9,18 +9,17 @@ function ContactsLoc({ job }: IJob) {
   return (
     <div>
       <section className="text-left max-w-[372px] text-lg text-grey-text xl:w-[400px] ">
-        <h2 className=" pb-4 mb-4 text-lg text-start font-bold text-grey-dark border-b border-grey-dark border-opacity-[0.13]  xl:hidden ">
-          Contacts
-        </h2>
+        <h2 className="section-title xl:hidden ">Contacts</h2>
 
         <div className="flex mx-auto bg-[#2A3047] rounded-lg  text-base text-[#e7eaf0]">
-          <div className=" self-center">
+          <div className="self-center">
             {/* ==================address======================= */}
             <address className="mx-16 my-8 leading-6 not-italic">
               <p className="font-bold mb-4">{job.name}</p>
+
               <p className="flex items-center ">
                 <MapPinIcon className="w-5 h-7 mr-1 " />
-                <a href="#">{job.address}</a>
+                <span>{job.address}</span>
               </p>
               <p>
                 <a href={`tel:${job.phone}`}>{job.phone}</a>
--- src/components/DetailsInfo/Share.tsx
+++ src/components/DetailsInfo/Share.tsx
@@ -3,10 +3,10 @@ import { ShareIcon } from "@heroicons/react/24/outline";
 function Share() {
   return (
     <div className=" ml-9 text-left  text-base text-grey-text xl:ml-6">
-      <a className="flex " href="#">
+      <p className="flex ">
         <ShareIcon className=" w-5 h-5 inline stroke-2 mr-3 xl:w-6 xl:h-6" />
         <span>Share</span>
-      </a>
+      </p>
     </div>
   );
 }
