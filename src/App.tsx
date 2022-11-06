ad0b80b95738eca28f292690797f1b6b7780cd6f
Max-Loktionov <studreno@gmail.com>
2022-11-06 19:54:16 +0200

    add all dir and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PropagateLoader from "react-spinners/PropagateLoader";
+import { JobIFace } from "./helpers/jobIFace";
+import { fetchJobList } from "./services/API";
+import "./App.css";
+
+const JobBoard = lazy(() => import("./pages/JobBoard"));
+const JobDetailed = lazy(() => import("./pages/JobDetailed"));
+const NotFoundPage = lazy(() => import("./pages/NotFound"));
 
 function App() {
+  const [data, setData] = useState<JobIFace[]>([]);
+
+  async function getJobList() {
+    const response = await fetchJobList();
+    setData(response);
+  }
+
+  useEffect(() => {
+    getJobList();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Suspense fallback={<PropagateLoader color="#41d61f" />}>
+          <Routes>
+            <Route path="/" element={<JobBoard items={data} />} />
+            <Route path="/:jobId" element={<JobDetailed data={data} />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </BrowserRouter>
   );
 }
 
